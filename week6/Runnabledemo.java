19904dbc50274042688a442f6d82e720ee2df5a9
Cosjimmy <Cosjimmy@users.noreply.github.com>
2016-10-15 18:18:17 -0400

    Add files via upload

---
+++ week6/Runnabledemo.java
@@ -0,0 +1,12 @@
+public class Runnabledemo implements Runnable  {
+public static final int COOLDOWN=500;//time for hold
+
+private Thread t;
+private String threadName;
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
